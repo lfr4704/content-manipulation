51747ddc579eb00afc26ef77efa8a32818f5230c
Luis <lfr4704@gmail.com>
2018-02-27 12:01:09 -0800

    revised

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var names = ["Irene Flores", "Rojin Jinro", "Jade Manrique", "Ed Dong", "Luis Robles", "David Escalante", "Patrick Porcuna"];
+
+var emails = ["irene.flores@gmail.com", "ro_jin25@icloud.com", "jade.marique@gmail.com","edtihcode@gmail.com", "lfr4704@gmail.com", "david.rubio@gmail.com", "porcuna.patrickj@gmail.com"];
+
+for (i = 0; i < names.length; i++) {
+	var html = "<div class =\"contact\">" + 
+				"<p>Name: " + names[i] + "</p>" + 
+	 			 "<p>Email: " + emails[i] + "</p>" 
+	 			 + "</div>";
+	document.getElementById("text").insertAdjacentHTML('beforeend', html);
+	
+	console.log(names[i]);
+	console.log(emails[i]);
+}
+
